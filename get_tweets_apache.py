9b47fb31ab4159286b9c666776a6ad8a8e500b39
Kimberly Kent <100380154+kimekent@users.noreply.github.com>
2022-12-24 14:00:32 +0100

    Update get_tweets_apache.py

--- get_tweets_apache.py
+++ get_tweets_apache.py
@@ -7,14 +7,14 @@ import pandas as pd
 # Function that is run by get tweets dag
 def run_get_tweets():
     # Twitter Credentials
-    client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAABywiAEAAAAAfYcaK69%2Bs1m9LTMAr2rxv54scWM%3DXMQMOYL4K5CG9y10wRAUMwaLZCASaad1iQESXzcYpgXSvmPMHZ')
+    client = tweepy.Client(bearer_token = credentials[bearer_token])
     auth = tweepy.OAuthHandler("IpWL6YCf7acBj8BrJPcDZ7SmH", "ANaBxuDPbFSZPlrI3v94TPQYXHcDuNkDG7T54sftQCibCSIxLI")
     auth.set_access_token("1580960869172887557-9Ui3It9gwYiv6UeU2wXn6ZSN9Uz2dB", "61BdNnKj17jMas8K4ksP8b7ReY1Wdqh6FJpfRe3Nwajms")
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # Connect to RDS in data lake and create twitter table
     try:
-        conn = psycopg2.connect("host=kimstestdb.cujm2drdr40t.us-east-1.rds.amazonaws.com dbname=kimstestdb user=postgres password=315096KEK")
+        conn = psycopg2.connect("host=dlcredentials["host"] dbname=dlcredentials["dbname"] user=dlcredentials["user"] password=dlcredentials["pw"]")
 
     except psycopg2.Error as e:
         print("Error: Could not make connection to the Postgres database")
@@ -70,46 +70,3 @@ def run_get_tweets():
 
     cur.close()
     conn.close()
-
-    # SQL Commands
-    #Show all tables
-    cur2.execute("""SELECT table_name FROM information_schema.tables
-           WHERE table_schema = 'public'""")
-    for table in cur2.fetchall():
-        print(table)
-
-    # Print all rows in a table
-    postgreSQL_select_Query = "select * from weatherforecast"
-    cur.execute(postgreSQL_select_Query)
-    tweets = cur.fetchall()
-    for row in tweets:
-        print(row)
-
-    dropTableStmt = "DROP TABLE %s;" % "tweets";
-    cur2.execute(dropTableStmt)
-
-    count_rows = """ SELECT COUNT (*) FROM tweets;"""
-    cur.execute(count_rows)
-    table_count = cur.fetchone()
-    print(table_count)
-
-# Delete all rows except cardiff
-DELETE = """DELETE FROM tweets
-            WHERE col5 != 'Cardiff' """
-cur.execute(DELETE)
-
-get_column_name ="""SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = N'capitals'"""
-
-change_column_name = """ALTER TABLE tweets
-  RENAME COLUMN 0 TO text;"""
-cur.execute(change_column_name)
-
-cur2.execute("Select * FROM costlivingprices LIMIT 0")
-colnames = [desc[0] for desc in cur.description]
-colnames
-
-pd.set_option('display.max_columns', None)
-data = pd.DataFrame(tweets)
-data.head()
